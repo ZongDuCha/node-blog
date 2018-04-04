1cc060a689f09156ea53e8ccf87a15b97cc08044
ZongDuCha <1367243169@qq.com>
2018-04-04 12:49:18 +0800

    blog

--- app.js
+++ app.js
@@ -21,11 +21,12 @@ app.set('view engine','html')
 app.use(bp.urlencoded({ extended: true, }))
 // 允许参数json类型，否则axios使用请求 后端无法接受
 app.use(bp.json())
-
-
 // 设置cookie
 app.use(function(req,res,next){
     req.cookies = new cookies(req,res);
+    req.cookies.set({
+        maxAge:6000
+    })
     // 设置全局cookie信息
     req.userInfo = {}; 
     if(!!req.cookies.get('userInfo')){
@@ -36,48 +37,6 @@ app.use(function(req,res,next){
     req.server_path = path.join(__dirname);
     next();
 })
-
-
-
-//------
-
-//加载ueditor 模块  
-var ueditor = require("ueditor");  
-  
-//使用模块  
-app.use("/ueditor/ue",ueditor('http://localhost:4040',function (req, res, next) {
-    console.log(1)  
-    // ueditor 客户发起上传图片请求  
-    if (req.query.action === 'uploadimage') {  
-        var foo = req.ueditor;  
-  
-        var imgname = req.ueditor.filename;  
-  
-        var img_url = '/images/ueditor/';  
-        res.ue_up(img_url); //你只要输入要保存的地址 。保存操作交给ueditor来做  
-        res.setHeader('Content-Type', 'text/html');//IE8下载需要设置返回头尾text/html 不然json返回文件会被直接下载打开  
-    }  
-    //  客户端发起图片列表请求  
-    else if (req.query.action === 'listimage') {  
-        var dir_url = '/images/ueditor/';  
-        res.ue_list(dir_url); // 客户端会列出 dir_url 目录下的所有图片  
-    }  
-    // 客户端发起其它请求  
-    else {
-        // console.log('config.json')  
-        res.setHeader('Content-Type', 'application/json');  
-        res.redirect('/ueditor/jsp/config.json');  
-    }  
-}));
-
-
-//------
-
-
-
-
-
-
 
 // 定义不同路径的模块
 // 后台
